31fdeb2536474ab5b1422d0117677bb1829db773
theshaneyu <shaneyu07@gmail.com>
2017-03-02 14:23:43 +0800

    KemMongoCache class in process

--- temp_file/kem.py
+++ temp_file/kem.py
@@ -1,10 +1,12 @@
 from pymongo import MongoClient
 
 class KemMongoCache(object):
-    def __init__(self):
-        self.uri = 'mongodb://140.120.13.243:4444/'
+    def __init__(self, uri = 'mongodb://140.120.13.243:4444/'):
+        self.client = MongoClient(uri)
+        self.db = slef.client['database']
+        self.coll = self.db['dataset']
 
-    def main(slef):
+    def main(self):
 
 
 
