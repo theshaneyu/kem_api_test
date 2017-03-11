d56ca921b83a0c097b2529d94feb30de80d0182c
Shane Yu <shaneyu07@gmail.com>
2017-03-11 22:49:19 +0000

    views.py and KemMongoCache completed, kem.py comments added

--- Kem_Api/kem/views.py
+++ Kem_Api/kem/views.py
@@ -1,15 +1,12 @@
 from django.http import HttpResponse
 from django.http import JsonResponse
-from ..kem import KemMongoCache
+from KEM.kem import KemMongoCache
 
 
 def test(request):
     keyword = request.GET['keyword']
     num = request.GET['num']
     kemObject = KemMongoCache('mongodb://140.120.13.243:4444/')
-    
-    
+
     return JsonResponse(kemObject.getTerms(keyword, int(num)), safe = False)
-
-
-    # return HttpResponse("<h1>Hello World!</h1>")
+    # return HttpResponse("<h1>Test</h1>")
