8e575b68eee80822bd3c6c6b73c0770796a6c076
theshaneyu <shaneyu07@gmail.com>
2017-03-11 11:31:27 +0000

    KemMongoCache class completed

---
+++ kem.py
@@ -0,0 +1,63 @@
+import pprint
+import json
+
+
+class KemMongoCache(object):
+    def __init__(self, uri = 'mongodb://140.120.13.243:4444/'):
+        from gensim import models
+        from pymongo import MongoClient
+        self.client = MongoClient(uri)
+        self.db = self.client['database']
+        self.coll = self.db['kem_test_coll']
+        self.model = models.KeyedVectors.load_word2vec_format('med250.model.bin',binary=True)
+
+
+    def getTerms(self, query, num):
+        result = self.coll.find({'Term':query}, {'Result':1, '_id':False}).limit(1)
+        if result.count() == 0:
+            resultJson = self.getJsonResult(query, num)
+            self.insertMongo(query, resultJson)
+            return resultJson
+
+        return (list(result)[0])['Result']
+
+        # print(type(result)) # <class 'pymongo.cursor.Cursor'>
+        # print((list(result)[0])['Result'])
+
+
+    def insertMongo(self, queryTerm, resultList):
+        state = self.coll.insert({'Term':queryTerm, 'Result':resultList})
+        print(state)
+
+
+
+    def getJsonResult(self, queryStr, number):
+        from gensim.models import word2vec
+        try:
+            print('Running most_similar function')
+
+            res = self.model.most_similar(queryStr, topn = number) # most_similar return a list
+            print(queryStr + '相似詞前' + str(number) + '排序')
+            resJson = json.dumps(res)
+
+            return resJson
+
+        except Exception as e:
+            print(repr(e))
+
+
+    def main(self):
+        # self.queryTerm()
+        testInput = '日本'
+        # jsonResult = self.getJsonResult(testInput)
+        # self.insertMongo(testInput, jsonResult)
+        temp = self.getTerms(testInput, 100)
+        print(type(temp))
+
+
+
+
+
+if __name__ == '__main__':
+    obj = KemMongoCache()
+    obj.main()
--- temp_file/kem.py
+++ temp_file/kem.py
@@ -1,27 +1,54 @@
-from pymongo import MongoClient
 import pprint
+import json
+from gensim import models
+from gensim.models import word2vec
 
 
 class KemMongoCache(object):
     def __init__(self, uri = 'mongodb://140.120.13.243:4444/'):
+        from pymongo import MongoClient
         self.client = MongoClient(uri)
         self.db = self.client['database']
         self.coll = self.db['kem_test_coll']
+        self.model = models.KeyedVectors.load_word2vec_format('med250.model.bin',binary=True)
 
 
-    def queryTerm(self):
+    def testQueryTerm(self):
         for item in self.coll.find():
             print(item)
 
 
+    def testInsert(self):
+        testData = {'name':'Eating', 'age':'13'}
+        result = self.coll.insert(testData)
+        print(result)
+
+
+    def resultToJson(self):
+        # print('Loading model...')
+        # model = models.KeyedVectors.load_word2vec_format('med250.model.bin',binary=True)
+
+        try:
+            print('Running most_similar function')
+
+            res = self.model.most_similar('中興大學',topn = 100)
+            print("中興大學相似詞前 100 排序")
+            # print(res)
+            # for item in res:
+                # print(item[0]+","+str(item[1]))
+                # print(item)
+                # break
+            resJson = json.dump(res)
+            print(resJson)
+
+        except Exception as e:
+            print(repr(e))
 
 
 
     def main(self):
-        # testData = {'name':'Eating', 'age':'13'}
-        # result = self.coll.insert(testData)
-        # print(result)
-        self.queryTerm()
+        # self.queryTerm()
+        self.resultToJson()
 
 
 
